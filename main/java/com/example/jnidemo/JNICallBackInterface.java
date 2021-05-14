a3a3d2a0ffd5e83267447e1bf105151252cae66a
alokkumar-github <alokkumar.here@gmail.com>
2021-05-14 13:32:40 +0530

    Add files via upload

---
+++ main/java/com/example/jnidemo/JNICallBackInterface.java
@@ -0,0 +1,6 @@
+package com.example.jnidemo;
+
+public interface JNICallBackInterface {
+
+    void callBackEvent(String data);
+}
---
+++ main/java/com/example/jnidemo/NativeLibrary.java
@@ -0,0 +1,33 @@
+package com.example.jnidemo;
+
+class NativeLibrary {
+
+    JNICallBackInterface callBackInterface;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    public NativeLibrary(JNICallBackInterface callBackInterface) {
+        this.callBackInterface = callBackInterface;
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public native int passingDataToJni(double[] tmpArray, int tmpInt, float tmpFloat);
+
+    public native int passObjectToJNI(SampleDataObj sampleDataObj);
+
+    public native SampleDataObj getObjectFromJNI();
+
+    public native void callTheCallBackMethod();
+
+    private void callBack(int data, String stringValue) {
+        callBackInterface.callBackEvent(stringValue + String.valueOf(data));
+    }
+}
